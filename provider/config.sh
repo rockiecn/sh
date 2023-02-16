e2a13ce4190259af55512cb02d401c7389490a2b
wy <wy@wy.com>
2023-02-16 16:38:05 +0800

    fix config.sh

--- provider/config.sh
+++ provider/config.sh
@@ -5,3 +5,8 @@ MEFS_PATH=~/work/p1/.memo-provider ./mefs-provider config set --key=contract.ver
 MEFS_PATH=~/work/p1/.memo-provider ./mefs-provider config set --key=contract.endPoint --value="https://testchain.metamemo.one:24180"
 
 MEFS_PATH=~/work/p1/.memo-provider ./mefs-provider config set --key=contract.roleContract --value="0x3c9ebF885d38813632FD129f892E1E0b75371cfA"
+
+MEFS_PATH=~/work/p1/.memo-provider ./mefs-provider bootstrap clear
+
+MEFS_PATH=~/work/p1/.memo-provider ./mefs-provider bootstrap add "/ip4/103.39.231.220/tcp/54001/p2p/12D3KooWNDQmR5ZsUWJeazcPS6TsVnAFofop4eZhDimDwqTZgQeq"
+
--- user/config.sh
+++ user/config.sh
@@ -5,3 +5,8 @@ MEFS_PATH=~/work/u1/.memo-user ./mefs-user config set --key=contract.version --v
 MEFS_PATH=~/work/u1/.memo-user ./mefs-user config set --key=contract.endPoint --value="https://testchain.metamemo.one:24180"
 
 MEFS_PATH=~/work/u1/.memo-user ./mefs-user config set --key=contract.roleContract --value="0x3c9ebF885d38813632FD129f892E1E0b75371cfA"
+
+MEFS_PATH=~/work/u1/.memo-user ./mefs-user bootstrap clear
+
+MEFS_PATH=~/work/u1/.memo-user ./mefs-user bootstrap add "/ip4/103.39.231.220/tcp/54001/p2p/12D3KooWNDQmR5ZsUWJeazcPS6TsVnAFofop4eZhDimDwqTZgQeq"
+
